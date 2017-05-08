b5b421958d42d5ace220d12714daa19a5334b007
stevederry <stevenwderry@gmail.com>
2017-05-07 21:52:31 -0400

    Update Derry1_Teleop_OpMode_Linear_v01.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Derry1_Teleop_OpMode_Linear_v01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Derry1_Teleop_OpMode_Linear_v01.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorSimple; //what is this and when would I use it?
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
 
