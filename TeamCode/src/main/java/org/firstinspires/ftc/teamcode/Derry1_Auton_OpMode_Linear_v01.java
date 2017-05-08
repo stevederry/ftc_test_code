d3804bd6220c2ffa873d81b9dafbc328aa9d84f6
stevederry <stevenwderry@gmail.com>
2017-05-08 10:35:15 -0400

    Update Derry1_Auton_OpMode_Linear_v01.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Derry1_Auton_OpMode_Linear_v01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Derry1_Auton_OpMode_Linear_v01.java
@@ -3,25 +3,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp; // NOT used
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@Autonomous(name="Derry1_Auton_OpMode_Linear_v01", group="Linear Opmode")
-
+// DEFINE OpMode
+// type(name,group) 
+@Autonomous(name="Derry1_Auton_OpMode_Linear_v01", group="Linear Opmode")    
+//
+// DEFINE Class
+// access, class name, name of class this new class extends
 public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
-
-    /* Declare OpMode members. */
-    private ElapsedTime runtime = new ElapsedTime(); // when use private, public, or leave blank?
+    //
+    // DECLARE OpMode MEMBERS
+    private ElapsedTime runtime = new ElapsedTime();        // when use private, public, or nothing?
     DcMotor leftDriveMotor = null;
     DcMotor rightDriveMotor = null;
     DcMotor spinnerMotor = null;
     Servo gripperServo= null;
 
     @Override       // what does OVERRIDE do/mean?
-    public void runOpMode() throws InterruptedException  { //what is interrupted exception?
+    public void runOpMode() throws InterruptedException  {  //what is "interrupted exception"?
         // display status and OpMode name on controller phone
         telemetry.addData("Status", "Initialized", "name");
         telemetry.update();
@@ -42,8 +45,8 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
         spinnerMotor.setDirection(DcMotor.Direction.FORWARD);
         //
         // SET ALL MOTORS TO DESIRED STARTING STATUS
-        robotStop();                    // Use method call to set all DC motors to STOP
-        gripperServo.setPosition(100);  // Set SERVO motor to desired address
+        robotStop();                        // Use method call to set all DC motors to STOP
+        gripperServo.setPosition(100);      // Set SERVO motor to desired address
         //
         // DEFINE CODE VARIABLES AND BEGINNING VALUES
         //
@@ -51,6 +54,7 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
         //
         // Drive times: all values are in milliseconds
         public static final double DriveTimeToCapBall = 10000;
+        public static final double DriveTimeCapBallToBase = 2000;
         public static final double DriveTime45DegTurn = 500;
         public static final double DriveTime90DegTurn = DriveTime45DegTurn*2;
         //
@@ -63,17 +67,28 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
         //
         // WAIT for driver to press PLAY
         waitForStart();
+        //
         ///////////////////////////////////////////////////////////////
         // AFTER driver presses PLAY, execute code below this line
         ///////////////////////////////////////////////////////////////
         //
         // Drive to Cap Ball, then stop
-        driveForward(DriveTimeToCapBall,DrivePowerFast);
-        robotStop();
+        driveForward(DriveTimeToCapBall,DrivePowerFast);    // arguments MUST be in order expected by method
+        robotStop();                                        // stop allows Cap Ball to bounce/flex before rogbot moves again
         //
         // Pivot left to nudge Cap Ball off center vortex base, then stop
         pivotLeft(DriveTime45DegTurn,DrivePowerMedium);
-        robotStop();
+        robotStop();                                        // stop allows Cap Ball to bounce/flex before rogbot moves again
+        //
+        // Pivot right to prepare to park on center vortex base
+        pivotRight(DriveTime45DegTurn,DrivePowerMedium);
+        //
+        // Drive forward onto center vortex base, then stop
+        driveForward(DriveTimeCapBallToBase,DrivePowerSlow);    
+        robotStop();                                        // final stop until beginning of Teleop
+        //
+        // If any parts of robot need to be reporsitioned (arms, etc.) to prepare for Teleop,
+        // place that code here before next "}" character
     }
     ///////////////////////////////////////////////////////////////
     // END of AUTONOMOUS code 
@@ -90,21 +105,21 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
     }
     //
     //driveForward(Time,Power)
-    public void driveForward(Time,Power){
+    public void driveForward(double Time, double Power){
         leftDriveMotor.setPower(Power);
         rightDriveMotor.setPower(Power);
         sleep(Time);
     }
     //
     //pivotRight(Time,Power)
-    public void pivotRight(Time,Power){
+    public void pivotRight(double Time, double Power){
         leftDriveMotor.setPower(Power);
         rightDriveMotor.setPower(-Power);
         sleep(Time);
     }
     //
     //pivotLeft(Time,Power)
-    public void pivotLeft(Time,Power){
+    public void pivotLeft(double Time, double Power){
         leftDriveMotor.setPower(-Power);
         rightDriveMotor.setPower(Power);
         sleep(Time);
