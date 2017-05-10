357b8ac9bbf85138db35c99a57a2a073d6abfdf8
stevederry <stevenwderry@gmail.com>
2017-05-10 08:11:40 -0400

    Update Derry1_Auton_OpMode_Linear_v01.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Derry1_Auton_OpMode_Linear_v01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Derry1_Auton_OpMode_Linear_v01.java
@@ -17,18 +17,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
     //
     // DECLARE OpMode MEMBERS
-    private ElapsedTime runtime = new ElapsedTime();        // when use private, public, or nothing?
+    private ElapsedTime runtime = new ElapsedTime();
     DcMotor leftDriveMotor = null;
     DcMotor rightDriveMotor = null;
     DcMotor spinnerMotor = null;
     Servo gripperServo= null;
 
     // Constants should generally be defined outside of
-    // method bodies (especially if you ever want to access
-    // them outside of this class)
-    // DEFINE CODE VARIABLES AND BEGINNING VALUES
+    //   method bodies (here) instead of below (inside runOpMode())
+    //   (especially if you ever want to access them outside of this class)
     //
-    // what does public static final double mean?
+    // DEFINE CODE VARIABLES AND BEGINNING VALUES
     //    public means it can be accessed from other classes
     //    final means its value never changes (constant)
     //    static means there is only one copy no matter how many instances of the class you create
@@ -44,16 +43,16 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
     public static final double DrivePowerMedium = .5;
     public static final double DrivePowerSlow = .2;
 
-    @Override       // what does OVERRIDE do/mean?
-    public void runOpMode() throws InterruptedException  {  //what is "interrupted exception"?
+    @Override                                                       // what does OVERRIDE do/mean?
+    public void runOpMode() throws InterruptedException  {          // what is "interrupted exception"?
         // display status and OpMode name on controller phone
         telemetry.addData("Status", "Initialized", "name");
         telemetry.update();
         //
         // INITIALIZE HARDWARE VARIABLES
-        // Values after 'get' MUST match EXACTLY the names used when the
-        // robot configuration was built using the FTC Robot Controler app
-        // on the robot controller phone
+        //   Values after 'get' MUST match EXACTLY the names used when the
+        //   robot configuration was built using the FTC Robot Controler app
+        //   on the robot controller phone
         leftDriveMotor = hardwareMap.dcMotor.get("leftDriveMotor");
         rightDriveMotor = hardwareMap.dcMotor.get("rightDriveMotor");
         spinnerMotor = hardwareMap.dcMotor.get("spinnerMotor");
@@ -61,13 +60,13 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
         //
         // SET MOTOR DIRECTIONS
         // "Reverse" any motor that runs backwards (relative to robot) when powered by positive value
-        leftDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftDriveMotor.setDirection(DcMotor.Direction.REVERSE);     // are ALL CAPS required?
         rightDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         spinnerMotor.setDirection(DcMotor.Direction.FORWARD);
         //
         // SET ALL MOTORS TO DESIRED STARTING STATUS
-        robotStop();                        // Use method call to set all DC motors to STOP
-        gripperServo.setPosition(100);      // Set SERVO motor to desired address
+        robotStop();                                                // use method call to set all DC motors to STOP
+        gripperServo.setPosition(100);                              // set SERVO motor to desired address
         //
         //
         // END OF PREPARATIONS
@@ -94,32 +93,35 @@ public class Derry1_Auton_OpMode_Linear_v01 extends LinearOpMode {
         driveForward(DriveTimeCapBallToBase,DrivePowerSlow);    
         robotStop();                                        // final stop until beginning of Teleop
         //
-        // If any parts of robot need to be reporsitioned (arms, etc.) to prepare for Teleop,
-        // place that code here before next "}" character
+        // If any parts of robot need to be repositioned (arms, etc.) to prepare for Teleop,
+        //   place that code here before next "}" character
     }
     ///////////////////////////////////////////////////////////////
     // END of AUTONOMOUS code 
     ///////////////////////////////////////////////////////////////
     //
     // DEFINE ALL METHODS
+    //   Methods are small sections of code that are written once but can be used ("called")
+    //   by the program multiple times. A method can have all of its values set internally (see robotStop, below)
+    //   or use values passed to it each time the program calls the method (see driveForward, below). Variables allow the
+    //   method's code to be written once but adapt to different situations in the section of code that is calling it.
     //
     // robotStop()
     // stop all motors at current location by setting all power to zero
-    public void robotStop(){
-        leftDriveMotor.setPower(0);
+    public void robotStop(){                                // the empty "()" section means that this method
+        leftDriveMotor.setPower(0);                         //   does not rely on variables
         rightDriveMotor.setPower(0);
         spinnerMotor.setPower(0);
     }
     //
     //driveForward(Time,Power)
-    public void driveForward(double Time, double Power){
-        leftDriveMotor.setPower(Power);
-        rightDriveMotor.setPower(Power);
-
-        // if you want this to be in seconds and not milliseconds
-        // try:
-        //  sleep((long) (Time * 1000.0));
-        sleep((long) Time);
+    public void driveForward(double Time, double Power){    // the variable names Time and Power will be assigned
+                                                            //   to the values passed into the method, in the order
+                                                            //   they are received
+        leftDriveMotor.setPower(Power);                     // run motor with passed Power value
+        rightDriveMotor.setPower(Power);                    // run motor with passed Power value
+        sleep((long) Time);                                 // wait here in code for duration of passed Time value,
+                                                            //   (allows motors to turn for duration of Time)
     }
     //
     //pivotRight(Time,Power)
