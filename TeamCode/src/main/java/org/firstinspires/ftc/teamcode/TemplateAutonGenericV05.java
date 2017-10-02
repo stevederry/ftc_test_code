ea33080133bb6ce6dc57bc170580f2aeba74c050
Steve Derry <stevenwderry@gmail.com>
2017-10-02 07:44:06 -0400

    Update TemplateAutonGenericV05.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateAutonGenericV05.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateAutonGenericV05.java
@@ -7,8 +7,8 @@
 //              1. Start at predetermined location (positioned by drivers prior to game start)
 //              2. Drive forward to make contact with game object, then pause to let object flex/bounce/roll/slide
 //              3. Spin left to push object off its original position, then pause to let object flex/bounce/roll/slide
-//              4. Spin right to return to original orientation and prepare to park on object's oroginal location
-//              5. Drive forward onto object's oroginal location, then stop
+//              4. Spin right to return to original orientation and prepare to park on object's original location
+//              5. Drive forward onto object's original location, then stop
 //              6. If any parts of robot need to be repositioned (arms, etc.) to prepare for Teleop,
 //                 place that code after step 5 and before next "}" character
 //              7. Wait for Teleop
@@ -20,45 +20,49 @@ package org.firstinspires.ftc.teamcode;
 // OpModes (specific)
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
 // Hardware types (ONE import per TYPE of hardware, NOT for each INSTANCE of that type of hardware)
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+//
 // Utilities (specific)
 import com.qualcomm.robotcore.util.ElapsedTime;
 // 
 // DEFINE OpMode
-// @type(name,group) 
+// FORMAT: @type(name,group) 
 @Autonomous(name="TemplateAutonGenericV05", group="Derry_FTC_Templates")  
 //
 // DEFINE Class
-// access level, class name, name of class this new class extends (if any)
+// FORMAT: access level, class name, name of class this new class extends (if any)
 public class TemplateAutonGenericV05 extends LinearOpMode {
     //
     // DECLARE OpMode MEMBERS
     //   Utilities
-    //   access level, utility name, starting value
+    //   FORMAT: access level, utility name, starting value
     private ElapsedTime runtime = new ElapsedTime();        // Use private unless you need access from other classes.
     //
     //   Hardware
-    //   hardware type, specific name of hardware, starting value
+    //   FORMAT: hardware type, specific name of hardware, starting value
     DcMotor leftDriveMotor = null;                          // One line for each hardware item
     DcMotor rightDriveMotor = null;                         // Values before '=' MUST match EXACTLY the names used when the
     DcMotor sweeperMotor = null;                            //   robot configuration was built using the FTC Robot Controler app
     Servo gripperServo= null;                               //   on the robot controller phone
     Servo sweeperServo= null;                               
+    //
     //   Constants should generally be defined outside of
     //     method bodies (here) instead of below (inside runOpMode()),
     //     especially if you ever want to access them outside of this class
     //
     // DEFINE CODE CONSTANTS, VARIABLES, AND BEGINNING VALUES
+    // FORMAT: access level, static yes/no, constant yes/no, value type, value name, beginning value
     //   public means it can be accessed from other classes
     //   static means there is only one copy no matter how many instances of the class you create
     //   final means its value never changes (constant)
     //   long is the type of value held by the variable
     //
     // Drive times: all values are in milliseconds
-    public static final long DRIVE_TIME_TO_CAP_BALL = 10000;
-    public static final long DRIVE_TIME_TO_CAP_BALL_TO_BASE = 2000;
+    public static final long DRIVE_TIME_TO_OBJECT = 10000;
+    public static final long DRIVE_TIME_TO_OBJECT_TO_BASE = 2000;
     public static final long DRIVE_TIME_45_DEG_TURN = 500;
     public static final long DRIVE_TIME_90_DEG_TURN = DRIVE_TIME_45_DEG_TURN * 2;
     //
@@ -67,22 +71,20 @@ public class TemplateAutonGenericV05 extends LinearOpMode {
     public static final double DRIVE_POWER_MEDIUM = .5;
     public static final double DRIVE_POWER_SLOW = .2;
     //
-    //  ?????????? Should the INITIALIZE HARDWARE VARIABLES and/or SET MOTOR DIRECTIONS sections
-    //  ??????????   below move to here to be grouped with other variables?
-    //
     @Override
     // Override is a note to the compiler, that you expect that you are replacing a method
     //   with the same name from the parent (extends XXX class). That way if you typo/change
     //   the method signature you will get an error.
     //
     // call runOpMode() method from the parent class of LinearOpMode
-    public void runOpMode() throws InterruptedException  {              // ?????????? what is "interrupted exception"?
+    public void runOpMode() throws InterruptedException  {              // "interrupted exception" keeps the program 
+                                                                        //    from freezing completely if there is an error
         // display status and OpMode name on controller phone
-        telemetry.addData("Status", "Initialized", "name"); // specific info to send to controller phone
-                                                            // ?????????? where does "name" come from?
-        telemetry.update();                                 // send info now
+        telemetry.addData("Status", "Initialized", "name");             // specific info to send to controller phone
+        telemetry.update();                                             // send info now
         //
         // INITIALIZE HARDWARE VARIABLES
+        // FORMAT: hardware name to be used by code, hardware designation in hardwaremap
         // Values after 'get' MUST match EXACTLY the names used when the
         //   robot configuration was built using the FTC Robot Controler app
         //   on the robot controller phone
@@ -115,27 +117,28 @@ public class TemplateAutonGenericV05 extends LinearOpMode {
         // AFTER driver presses PLAY, execute code below this line
         ///////////////////////////////////////////////////////////////
         //
-        // 1. Start at midpoint of wall (positioned by drivers prior to game start)
+        // 1. Start at predetermined location (positioned by drivers prior to game start)
         //
-        // 2. Drive forward to Cap Ball, making contact, then pause to let Cap Ball flex/bounce/roll
-        driveForward(DRIVE_TIME_TO_CAP_BALL,DRIVE_POWER_FAST);    // Arguments MUST be in order expected by method
-        stopRobot();                                        // Stop then sleep allows Cap Ball to bounce/flex before rogbot moves again        
+        // 2. Drive forward to make contact with game object, then pause to let object flex/bounce/roll/slide
+        driveForward(DRIVE_TIME_TO_OBJECT,DRIVE_POWER_FAST);    // Arguments MUST be in order expected by method
+        stopRobot();                                        // Stop then sleep allows Object to bounce/flex before rogbot moves again        
         sleep((long) 2);                                    // 2 seconds
         //
-        // 3. Spin left to push Cap Ball off center vortex base, then pause to let Cap Ball flex/bounce/roll
+        // 3. Spin left to push object off its base, then pause to let object flex/bounce/roll
         spinLeft(DRIVE_TIME_45_DEG_TURN,DRIVE_POWER_MEDIUM);      // Spin 45 deg. to left
-        stopRobot();                                        // Stop then sleep allows Cap Ball to bounce/flex before rogbot moves again        
+        stopRobot();                                        // Stop then sleep allows Object to bounce/flex before rogbot moves again        
         sleep((long) 2);                                    // 2 seconds
         //
-        // 4. Spin right to prepare to park on center vortex base
+        // 4. Spin right to prepare to park on base
         spinRight(DRIVE_TIME_45_DEG_TURN,DRIVE_POWER_MEDIUM);     // Spin 45 deg. to right to return to original orientation
         //
-        // 5. Drive forward onto center vortex base, then stop
-        driveForward(DRIVE_TIME_TO_CAP_BALL_TO_BASE,DRIVE_POWER_SLOW);    
+        // 5. Drive forward onto base, then stop
+        driveForward(DRIVE_TIME_TO_OBJECT_TO_BASE,DRIVE_POWER_SLOW);    
         stopRobot();                                        // Final stop until beginning of Teleop
         //
         // 6. If any parts of robot need to be repositioned (arms, etc.) to prepare for Teleop,
         //    place that code here before next "}" character
+        //
         // Open gripper hand
         gripperServo.setPosition(200);      // Set SERVO motor to desired address (200 is just an example; value depends on robot)
         // 7. Wait for Teleop
@@ -149,10 +152,6 @@ public class TemplateAutonGenericV05 extends LinearOpMode {
     //   by the program multiple times. A method can have all of its values set internally (see robotStop, below)
     //   or use values passed to it each time the program calls the method (see driveForward, below). Variables allow the
     //   method's code to be written once but adapt to different situations in the section of code that is calling it.
-    //
-    //   ?????????? Can/should METHODS be stored in a separate file so that more than one program file
-    //   ??????????  can access them? And so that changes to those METHODS can be made in one place rather
-    //   ??????????  than in each instance of a "calling" program?
     //
     // METHOD stopRobot()
     //    stop all motors at current location by setting all power to zero
